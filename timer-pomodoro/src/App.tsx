d8e1a40d1b5b55c8a775abb41018a96cbaf91738
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-10-18 21:33:54 -0300

    Rotas  - com react-router-dom

--- timer-pomodoro/src/App.tsx
+++ timer-pomodoro/src/App.tsx
@@ -1,17 +1,17 @@
-import { Button } from './components/Button/Button'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
 import { defaultTheme } from './themes/styles/default'
 import { GlobalStyle } from './themes/styles/Global'
+import { Router } from './components/Router/Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button color="primary" />
-      <Button color="secondary" />
-      <Button color="danger" />
-      <Button color="sucess" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
