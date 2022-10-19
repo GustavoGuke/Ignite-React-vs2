b677c56648877659c41dd9c39e4cca0fcb765359
Gustavo da Silva de Lima <gustavo.lima@poleoduto.com.br>
2022-10-19 17:28:05 -0300

    Pagina home

---
+++ timer-pomodoro/src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import { Play } from "phosphor-react";
+
+export function Home() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou Trabalhar em</label>
+          <input type="task" />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </div>
+  )
+}
