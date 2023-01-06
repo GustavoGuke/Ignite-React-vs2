c7ca00082e42785a512a0ec904da2a7de7699f0b
Gustavo Silva <gustavo.athenas13@gmail.com>
2023-01-05 22:14:27 -0300

    Botões de entrada e saida - acessibilidade btn

--- gt-money/src/components/NewTransactionModal/index.tsx
+++ gt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { Close, Content, Overlay } from './style';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { Close, Content, Overlay, TransactionType, TransactionTypeButton } from './style';
 export function NewTransactionModal() {
     return (
         <Dialog.Portal>
@@ -14,7 +14,10 @@ export function NewTransactionModal() {
                     <input type="text" placeholder='Descrição' required />
                     <input type="number" placeholder='Preço' required />
                     <input type="text" placeholder='Categoria' required />
-
+                    <TransactionType>
+                        <TransactionTypeButton variant='income' value='income'><ArrowCircleUp size={24}/> Entrada</TransactionTypeButton>
+                        <TransactionTypeButton variant='outcome' value='outcome'><ArrowCircleDown size={24}  /> Saida </TransactionTypeButton>
+                    </TransactionType>
                     <button type='submit'>Cadastrar</button>
                 </form>
                 
